98e8246b2d4c219dc3ed1d44a8018560bce69f97
shanmukh-v-i <shanmukh.v.i@gmail.com>
2020-12-22 00:07:44 +0530

    Commit Springboot

---
+++ src/main/java/com/mphasis/spring/dao/BookDao.java
@@ -0,0 +1,20 @@
+package com.mphasis.spring.dao;
+
+import java.util.List;
+
+import com.mphasis.spring.model.Book;
+
+
+public interface BookDao {
+
+   long save(Book book);
+
+   Book get(long id);
+
+   List<Book> list();
+
+   void update(long id, Book book);
+
+   void delete(long id);
+
+}
